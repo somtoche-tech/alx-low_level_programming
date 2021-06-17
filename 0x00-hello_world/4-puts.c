83fe39c9da3c171d721650440893a756068c20f7
somtoche-tech <igbokwetoche@gmail.com>
2021-06-17 11:11:58 -0700

    C program to print a quote

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+\**
+ *This program prints out the words "Programming is like building a MULTILINGUAL PUZZLE"
+*/
+
+int main(void)
+{
+  puts("\"Programming is like a multilingual puzzle");
+  return 0;
+}
