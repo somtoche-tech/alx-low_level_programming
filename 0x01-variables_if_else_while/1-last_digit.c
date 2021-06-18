1e2659ef3da1eb5d20fb856b120919c774138464
somtoche-tech <igbokwetoche@gmail.com>
2021-06-18 14:00:06 -0700

    last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -8,8 +8,9 @@
 
 /**
  * main - this program prints the last digit of n
- * and prints out whether it is greater than 5, 0 or less than 6
- * return: (0) Always
+ * and prints out whether it is greater than 5, 0 or
+ * less than 6
+ * Return: (0)
  */
 
 int main(void)
