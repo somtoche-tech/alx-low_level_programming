e36b2af64d407f74608d6a643c1547cc55c9141f
somtoche-tech <igbokwetoche@gmail.com>
2021-06-20 05:55:56 -0700

    Prints single digit numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - prints numbers 0-9
+ * Prints single digit numbers 0-9 in base 10
+ * Return: 0
+ */
+
+int main(void)
+{
+int x;
+for (x = 0; x <= 9; x++)
+putchar(x);
+putchar('\n');
+return (0);
+}
