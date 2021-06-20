bab018a58eed0ddd8ebcce199b89f5eb642f0ad3
somtoche-tech <igbokwetoche@gmail.com>
2021-06-20 04:24:10 -0700

    print the alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints out the alphabets in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+char a = 'a';
+while (a <= 'z')
+{
+putchar(a);
+a++;
+}
+putchar("\n");
+return (0);
+}
