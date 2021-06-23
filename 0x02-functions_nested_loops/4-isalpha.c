615ec3a128c9c0bbfa59cd83414fad44f72c3d5a
somtoche-tech <igbokwetoche@gmail.com>
2021-06-23 13:56:33 -0700

    checks alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - this returns 1 if c is lower case and returns 0 if not
+ *@c: this is the integer to test
+ * Return:1 or 0
+ */
+
+int _isalpha(int c)
+{
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+
+return (1);
+}
+return (0);
+}
