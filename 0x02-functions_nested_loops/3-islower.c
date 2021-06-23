1cb674a4c86b2e6013c967d2f6003ca4b3ce728d
somtoche-tech <igbokwetoche@gmail.com>
2021-06-23 13:38:11 -0700

    check to see if upper or lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _islower - checks to see if c is lower case, no pun intended
+ * @c: integer to be tested
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+return (c >= 97 && c <= 122);
+}
