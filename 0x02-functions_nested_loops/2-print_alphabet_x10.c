30bfe6d328d2cecf966c577fa90c8c11a956e04b
somtoche-tech <igbokwetoche@gmail.com>
2021-06-23 13:24:06 -0700

    prints alphabets ten times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - prints the alphabet ten times
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+int x;
+char y;
+for (x = 0; x < 10; x++)
+{
+for (y = 'a'; y <= 'z'; y++)
+{
+_putchar(y);
+}
+_putchar('\n');
+}
+}
